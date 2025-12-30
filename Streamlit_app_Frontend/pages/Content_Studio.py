c687e7988a2cb9892a8e280269c3d128ba83dbbb
Sparsh3359 <sparshh2003@gmail.com>
2025-12-30 12:52:13 +0530

    spelling check

--- Streamlit_app_Frontend/pages/Content_Studio.py
+++ Streamlit_app_Frontend/pages/Content_Studio.py
@@ -21,7 +21,7 @@ ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if ROOT_DIR not in sys.path:
     sys.path.insert(0, ROOT_DIR)
 
-from utils.auth_guard import protect
+from utils.auth_gaurd import protect
 
 # -------------------------------
 # ENABLE AUTH
